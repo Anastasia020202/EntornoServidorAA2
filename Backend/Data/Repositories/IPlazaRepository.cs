37ecc9788fdc99a2b57dbb7705f060c5ba823d81
Anastasia Singerean <a27138@svalero.com>
2025-09-06 00:17:01 +0200

    feat(data): añadir métodos básicos a IPlazaRepository

--- Backend/Data/Repositories/IPlazaRepository.cs
+++ Backend/Data/Repositories/IPlazaRepository.cs
@@ -1,6 +1,13 @@
+using ParkingApp2.Models;
+
 namespace ParkingApp2.Data.Repositories
 {
     public interface IPlazaRepository
     {
+        Plaza? GetPlazaById(int id);
+        IEnumerable<Plaza> GetPlazas();
+        Plaza AddPlaza(Plaza plaza);
+        void DeletePlaza(int id);
+        void SaveChanges();
     }
 }
