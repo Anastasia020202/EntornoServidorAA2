e9f0a6f87ee8189c1cb3dcf7100c9bdf7c52c773
Anastasia Singerean <a27138@svalero.com>
2025-09-05 23:52:41 +0200

    feat(models): añadir propiedad SaltContrasena a Usuario

--- Backend/Models/Usuario.cs
+++ Backend/Models/Usuario.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Correo { get; set; } = "";
         public string HashContrasena { get; set; } = "";
+        public byte[] SaltContrasena { get; set; } = Array.Empty<byte>();
         public string Rol { get; set; } = "User";
         
         public List<Reserva>? Reservas { get; set; }
