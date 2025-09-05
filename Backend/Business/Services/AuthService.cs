97789c4c4a9db63cf8d7915f06501ebbf2dce9a4
Anastasia Singerean <a27138@svalero.com>
2025-09-05 23:40:37 +0200

    feat(business): añadir constructor con IUsuarioRepository a AuthService

--- Backend/Business/Services/AuthService.cs
+++ Backend/Business/Services/AuthService.cs
@@ -1,6 +1,14 @@
+using ParkingApp2.Data.Repositories;
+
 namespace ParkingApp2.Business.Services
 {
     public class AuthService
     {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public AuthService(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
     }
 }
