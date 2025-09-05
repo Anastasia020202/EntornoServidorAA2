c7b44006b0f2f97dcce7e495ee54af54a9528e81
Anastasia Singerean <a27138@svalero.com>
2025-09-06 00:00:15 +0200

    feat(api): añadir endpoints GET a UsuariosController

--- Backend/API/Controllers/UsuariosController.cs
+++ Backend/API/Controllers/UsuariosController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ParkingApp2.Data.Repositories;
+using ParkingApp2.Models;
 
 namespace ParkingApp2.API.Controllers
 {
@@ -12,6 +13,25 @@ namespace ParkingApp2.API.Controllers
         public UsuariosController(IUsuarioRepository usuarioRepository)
         {
             _usuarioRepository = usuarioRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsuarios()
+        {
+            var query = new UsuarioQueryParameters();
+            var usuarios = _usuarioRepository.GetUsuarios(query);
+            return Ok(usuarios);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUsuarioById(int id)
+        {
+            var usuario = _usuarioRepository.GetUsuarioById(id);
+            if (usuario == null)
+            {
+                return NotFound(new { message = "Usuario no encontrado" });
+            }
+            return Ok(usuario);
         }
     }
 }
