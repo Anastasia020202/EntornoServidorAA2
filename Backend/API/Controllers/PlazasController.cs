7ee9f705175eadd56f902a6f2c3f8f454fd1d2bb
Anastasia Singerean <a27138@svalero.com>
2025-09-06 00:19:51 +0200

    feat(api): añadir endpoints GET a PlazasController

--- Backend/API/Controllers/PlazasController.cs
+++ Backend/API/Controllers/PlazasController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ParkingApp2.Data.Repositories;
+using ParkingApp2.Models;
 
 namespace ParkingApp2.API.Controllers
 {
@@ -12,6 +13,24 @@ namespace ParkingApp2.API.Controllers
         public PlazasController(IPlazaRepository plazaRepository)
         {
             _plazaRepository = plazaRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPlazas()
+        {
+            var plazas = _plazaRepository.GetPlazas();
+            return Ok(plazas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPlazaById(int id)
+        {
+            var plaza = _plazaRepository.GetPlazaById(id);
+            if (plaza == null)
+            {
+                return NotFound(new { message = "Plaza no encontrada" });
+            }
+            return Ok(plaza);
         }
     }
 }
