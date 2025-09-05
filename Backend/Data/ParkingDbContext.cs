bcc9963813d76889fb48f3226ac3098f38c889f3
Anastasia Singerean <a27138@svalero.com>
2025-09-05 22:51:37 +0200

    feat(data): añadir ParkingDbContext con DbSet

---
+++ Backend/Data/ParkingDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkingApp2.Models;
+
+namespace ParkingApp2.Data
+{
+    public class ParkingDbContext : DbContext
+    {
+        public ParkingDbContext(DbContextOptions<ParkingDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Plaza> Plazas { get; set; }
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+    }
+}
