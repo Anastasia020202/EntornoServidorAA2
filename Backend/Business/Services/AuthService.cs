69474cfcf5f6db7fe6c748c46a822493ad0ff376
Anastasia Singerean <a27138@svalero.com>
2025-09-05 23:41:25 +0200

    feat(business): implementar método HashPassword en AuthService

--- Backend/Business/Services/AuthService.cs
+++ Backend/Business/Services/AuthService.cs
@@ -1,4 +1,6 @@
 using ParkingApp2.Data.Repositories;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ParkingApp2.Business.Services
 {
@@ -9,6 +11,14 @@ namespace ParkingApp2.Business.Services
         public AuthService(IUsuarioRepository usuarioRepository)
         {
             _usuarioRepository = usuarioRepository;
+        }
+
+        public (string hash, byte[] salt) HashPassword(string password)
+        {
+            using var hmac = new HMACSHA256();
+            var salt = hmac.Key;
+            var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return (Convert.ToBase64String(hash), salt);
         }
     }
 }
