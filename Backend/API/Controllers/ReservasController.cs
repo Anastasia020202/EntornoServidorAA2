c59dc4b0d608952a5f6679668555c987d45dd0d5
Anastasia Singerean <a27138@svalero.com>
2025-09-06 00:42:18 +0200

    feat(api): añadir endpoints GET a ReservasController

--- Backend/API/Controllers/ReservasController.cs
+++ Backend/API/Controllers/ReservasController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ParkingApp2.Data.Repositories;
+using ParkingApp2.Models;
 
 namespace ParkingApp2.API.Controllers
 {
@@ -12,6 +13,45 @@ namespace ParkingApp2.API.Controllers
         public ReservasController(IReservaRepository reservaRepository)
         {
             _reservaRepository = reservaRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllReservas()
+        {
+            var reservas = _reservaRepository.GetReservas();
+            return Ok(reservas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetReservaById(int id)
+        {
+            var reserva = _reservaRepository.GetReservaById(id);
+            if (reserva == null)
+            {
+                return NotFound(new { message = "Reserva no encontrada" });
+            }
+            return Ok(reserva);
+        }
+
+        [HttpGet("usuario/{usuarioId}")]
+        public IActionResult GetReservasByUsuario(int usuarioId)
+        {
+            var reservas = _reservaRepository.GetReservasByUsuarioId(usuarioId);
+            return Ok(reservas);
+        }
+
+        [HttpGet("vehiculo/{vehiculoId}")]
+        public IActionResult GetReservasByVehiculo(int vehiculoId)
+        {
+            var reservas = _reservaRepository.GetReservasByVehiculoId(vehiculoId);
+            return Ok(reservas);
+        }
+
+        [HttpGet("plaza/{plazaId}")]
+        public IActionResult GetReservasByPlaza(int plazaId)
+        {
+            var reservas = _reservaRepository.GetReservasByPlazaId(plazaId);
+            return Ok(reservas);
         }
     }
 }
